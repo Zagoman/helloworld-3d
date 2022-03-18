a7cc0222ad1e5732b26efc3497e785ee1bfea920
Zagoman <lucaszago.ph@gmail.com>
2022-03-18 10:04:20 +0100

    final presentation

--- hello-world.js
+++ hello-world.js
@@ -13,8 +13,8 @@ const near = 0.1; //near tells the camera from how far from the lens you want to
 const far = 5.0; //far tells the camera when to stop
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); //The camera itself
 camera.position.z = 3;
-camera.position.x = 0;
-camera.position.y = 0;
+// camera.position.x = 0;
+// camera.position.y = 0;
 
 // Create a scene
 const scene = new THREE.Scene();
@@ -35,9 +35,9 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
 //Blend the geometry with the material using the Mesh
 const box = new THREE.Mesh(geometry, material);
-box.position.z = 0;
-box.position.x = 2;
-box.position.y = 0;
+// box.position.z = 0;
+box.position.x = -2;
+box.position.y = -1;
 
 /**
  * When you create a geometry, you need to add it to the scene for it to become visible
